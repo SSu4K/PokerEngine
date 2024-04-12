458d867478ca192e0e0f4d7d2b39407a6dc944df
SSu4K <ssudak.kontakt@gmail.com>
2024-04-13 00:54:22 +0200

    Added player repr.

--- player.py
+++ player.py
@@ -2,4 +2,7 @@ class Player:
     def __init__(self, name, money) -> None:
         self.name = name
         self.money = money
+    
+    def __repr__(self):
+        return str(vars(self))
         
